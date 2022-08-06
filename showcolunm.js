988061232f27380aae885973187c64b71020366c
Vangeance <ismaelmartins919@gmail.com>
2022-08-06 03:36:14 -0300

    deixando section skills flexivel

--- showcolunm.js
+++ showcolunm.js
@@ -1,26 +1,24 @@
 class Next{
-    constructor(btn, colunm, colunm2, colunm3){
+    constructor(btn, colunm){
         this.botao = document.querySelectorAll(btn)
-        this.coluna = document.querySelector(colunm)
-        this.coluna2 = document.querySelector(colunm2)
-        this.coluna3 = document.querySelector(colunm3)
+        this.coluna = document.querySelectorAll(colunm)
     }
 
     clique(){
         this.botao[0].onclick = () =>{
-            this.coluna.style.display = "none"
-            this.coluna2.style.display = "block"
+            this.coluna[0].style.display = "none"
+            this.coluna[1].style.display = "block"
         }
         this.botao[1].onclick = () =>{
-            this.coluna2.style.display = "none"
-            this.coluna3.style.display = "block"
+            this.coluna[1].style.display = "none"
+            this.coluna[2].style.display = "block"
         }
         this.botao[2].onclick = () =>{
-            this.coluna3.style.display = "none"
-            this.coluna.style.display = "block"
+            this.coluna[2].style.display = "none"
+            this.coluna[0].style.display = "block"
         }
     }
 }
 
-const next1 = new Next(".colunm-btn", ".primeira-coluna", ".segunda-coluna", ".terceira-coluna") 
+const next1 = new Next(".colunm-btn", ".coluna") 
 next1.clique()
