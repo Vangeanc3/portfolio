411da5a30e8bf42fafb41f95c172d5917c333195
Vangeanc3 <ismaelmartins919@gmail.com>
2022-12-19 12:03:27 -0300

    add envio de email portfolio

--- modal.js
+++ modal.js
@@ -1,4 +1,31 @@
-document.querySelector(".projetos-card-btn").addEventListener("click", () => {
-   let modal = document.querySelector(".modal-container")
-   modal.style.display = "flex"
+var btn1 = document.getElementById("projeto1");
+var btn2 = document.getElementById("projeto2");
+var btn3 = document.getElementById("projeto3");
+
+btn1.addEventListener("click", () => {
+   let modal1 = document.getElementById("modal1");
+   Flex(modal1)
+   None(modal1);
 })
+
+btn2.addEventListener("click", () => {
+   let modal2 = document.getElementById("modal2");
+   Flex(modal2)
+   None(modal2);
+})
+
+btn3.addEventListener("click", () => {
+   let modal3 = document.getElementById("modal3");
+   Flex(modal3)
+   None(modal3);
+})
+
+const None = (elemento) => {
+   elemento.addEventListener("click", () => {
+      elemento.style.display = "none";
+   })
+};
+
+const Flex = (elemento) => {
+      elemento.style.display = "flex";
+   }
