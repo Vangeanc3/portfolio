a0123d03e0929b20fd901c5f1e007b155ffab9cc
Vangeanc3 <ismaelmartins919@gmail.com>
2023-03-27 16:01:42 -0300

    UPDATE de conteudo

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,8 @@ const wrapper1 = document.getElementById("modal-wrapper1")
 const wrapper2 = document.getElementById("modal-wrapper2")
 const wrapper3 = document.getElementById("modal-wrapper3")
 
-const btnFechar = document.querySelector(".modal-fechar");
+const btnFechar1 = document.querySelector("#modal-fechar1");
+const btnFechar2 = document.querySelector("#modal-fechar2");
 
 // FUNÇÃO QUE VAI ABRIR O MODAL:
 btn1.addEventListener("click", () => {
@@ -20,6 +21,24 @@ btn1.addEventListener("click", () => {
    modal1.classList.add("visible");
    wrapper1.classList.add("modalAnimado");
    document.querySelector("body").style.overflowY = "hidden";
+})
+
+btn2.addEventListener("click", () => {
+   if (modal2.classList.contains("fechaModal")) {
+      modal2.classList.remove("fechaModal")
+   }
+   modal2.classList.add("visible");
+   wrapper2.classList.add("modalAnimado");
+   document.querySelector("body").style.overflowY = "hidden";
+})
+
+
+btn3.addEventListener("click", () => {
+   if (modal3.classList.contains("fechaModal")) {
+      modal3.classList.remove("fechaModal")
+   }
+   modal3.classList.add("visible");
+   wrapper3.classList.add("modalAnimado");
 })
 //FUNÇÃO QUE VAI FECHAR O MODAL QUANDO CLICADO FORA DELE:
 modal1.addEventListener("click", function (event) {
@@ -32,24 +51,6 @@ modal1.addEventListener("click", function (event) {
       document.querySelector("body").style.overflowY = "auto";
    }
 });
-//FUNÇÃO QUE VAI FECHAR O MODAL QUANDO CLICAR NO BOTÃO DE FECHAR:
-btnFechar.addEventListener("click", () => {
-   if (modal1.classList.contains("visible")) {
-      modal1.classList.add("fechaModal");
-      setTimeout(() => { modal1.classList.remove("visible") }, 1000)
-   }
-   wrapper1.classList.remove("modalAnimado");
-   document.querySelector("body").style.overflowY = "auto";
-})
-
-
-btn2.addEventListener("click", () => {
-   if (modal2.classList.contains("fechaModal")) {
-      modal2.classList.remove("fechaModal")
-   }
-   modal2.classList.add("visible");
-   wrapper2.classList.add("modalAnimado");
-})
 
 modal2.addEventListener("click", function (event) {
    if (event.target === modal2) {
@@ -58,16 +59,9 @@ modal2.addEventListener("click", function (event) {
          setTimeout(() => { modal2.classList.remove("visible") }, 1000)
       }
       wrapper2.classList.remove("modalAnimado");
+      document.querySelector("body").style.overflowY = "auto";
    }
 });
-
-btn3.addEventListener("click", () => {
-   if (modal3.classList.contains("fechaModal")) {
-      modal3.classList.remove("fechaModal")
-   }
-   modal3.classList.add("visible");
-   wrapper3.classList.add("modalAnimado");
-})
 
 modal3.addEventListener("click", function (event) {
    if (event.target === modal3) {
@@ -78,3 +72,23 @@ modal3.addEventListener("click", function (event) {
       wrapper3.classList.remove("modalAnimado");
    }
 });
+//FUNÇÃO QUE VAI FECHAR O MODAL QUANDO CLICAR NO BOTÃO DE FECHAR:
+btnFechar1.addEventListener("click", () => {
+   if (modal1.classList.contains("visible")) {
+      modal1.classList.add("fechaModal");
+      setTimeout(() => { modal1.classList.remove("visible") }, 1000)
+   }
+   wrapper1.classList.remove("modalAnimado");
+   document.querySelector("body").style.overflowY = "auto";
+})
+
+btnFechar2.addEventListener("click", () => {
+   if (modal2.classList.contains("visible")) {
+      modal2.classList.add("fechaModal");
+      setTimeout(() => { modal2.classList.remove("visible") }, 1000)
+   }
+   wrapper2.classList.remove("modalAnimado");
+   document.querySelector("body").style.overflowY = "auto";
+})
+
+
